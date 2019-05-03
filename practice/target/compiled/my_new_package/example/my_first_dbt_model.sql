2bdf681d576ef771ac5282719de9a2a717ef4c36
justinl102 <labrash.justin@gmail.com>
2019-05-03 14:24:14 -0400

    Add files via upload

---
+++ practice/target/compiled/my_new_package/example/my_first_dbt_model.sql
@@ -0,0 +1,14 @@
+-- Welcome to your first dbt model!
+-- Did you know that you can also configure models directly within
+-- the SQL file? This will override configurations stated in dbt_project.yml
+
+-- Try changing 'view' to 'table', then re-running dbt
+
+
+
+select *,
+       Concat(first_name,' ', last_name) as full_name
+
+
+
+from public.actor
